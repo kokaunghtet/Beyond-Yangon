df906cccf4592bdb741f8d9134a363b13ae80d20
Kaung Htet <kh622962@gmail.com>
2026-03-10 11:19:44 +0630

    Add initial map data configuration for Myanmar with state and location details

---
+++ mapdata.js
@@ -0,0 +1,238 @@
+var simplemaps_countrymap_mapdata = {
+  main_settings: {
+    //General settings
+    width: "responsive",
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    pop_ups: "detect",
+
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+  },
+  state_specific: {
+    MM01: {
+      name: "Sagaing",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM02: {
+      name: "Bago",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM03: {
+      name: "Magway",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM04: {
+      name: "Mandalay",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM05: {
+      name: "Tanintharyi",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM06: {
+      name: "Yangon",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM07: {
+      name: "Ayeyarwady",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM11: {
+      name: "Kachin",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM12: {
+      name: "Kayah",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM13: {
+      name: "Kayin",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM14: {
+      name: "Chin",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM15: {
+      name: "Mon",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM16: {
+      name: "Rakhine",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+    MM17: {
+      name: "Shan",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default",
+    },
+  },
+  locations: {
+    0: {
+      name: "Nay Pyi Taw",
+      lat: "19.745",
+      lng: "96.129722",
+    },
+  },
+  labels: {
+    MM01: {
+      name: "Sagaing",
+      parent_id: "MM01",
+    },
+    MM02: {
+      name: "Bago",
+      parent_id: "MM02",
+    },
+    MM03: {
+      name: "Magway",
+      parent_id: "MM03",
+    },
+    MM04: {
+      name: "Mandalay",
+      parent_id: "MM04",
+    },
+    MM05: {
+      name: "Tanintharyi",
+      parent_id: "MM05",
+    },
+    MM06: {
+      name: "Yangon",
+      parent_id: "MM06",
+    },
+    MM07: {
+      name: "Ayeyarwady",
+      parent_id: "MM07",
+    },
+    MM11: {
+      name: "Kachin",
+      parent_id: "MM11",
+    },
+    MM12: {
+      name: "Kayah",
+      parent_id: "MM12",
+    },
+    MM13: {
+      name: "Kayin",
+      parent_id: "MM13",
+    },
+    MM14: {
+      name: "Chin",
+      parent_id: "MM14",
+    },
+    MM15: {
+      name: "Mon",
+      parent_id: "MM15",
+    },
+    MM16: {
+      name: "Rakhine",
+      parent_id: "MM16",
+    },
+    MM17: {
+      name: "Shan",
+      parent_id: "MM17",
+    },
+  },
+};
